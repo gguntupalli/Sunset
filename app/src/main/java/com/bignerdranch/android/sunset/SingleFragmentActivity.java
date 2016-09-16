9ae43e38f07b35a014922b4a4b94387474ab911f
Gopi Guntupalli <gguntupalli@enova.com>
2016-09-16 17:42:40 -0500

    animations

---
+++ app/src/main/java/com/bignerdranch/android/sunset/SingleFragmentActivity.java
@@ -0,0 +1,37 @@
+package com.bignerdranch.android.sunset;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bignerdranch.android.sunset.R;
+
+/**
+ * Created by gguntupalli on 8/4/16.
+ */
+public abstract class SingleFragmentActivity  extends AppCompatActivity{
+
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
